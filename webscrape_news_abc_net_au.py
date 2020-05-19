092a595b76f28fa9b874a63dd3336df8c09a7ff4
RC Timmer <46539851+RoelTim@users.noreply.github.com>
2020-05-19 16:56:41 +1000

    Update webscrape_news_abc_net_au.py

--- webscrape_news_abc_net_au.py
+++ webscrape_news_abc_net_au.py
@@ -1,40 +1,41 @@
-#-*- coding: utf-8 -*-
-"""
-Webscrape text of the Australian news website www.abc.net.au/news
-@author: roelientimmer
-"""
-
 from bs4 import BeautifulSoup
 import requests
 import winsound
 
 URL_start = 'https://www.abc.net.au/news/'
 
-f = open("news.txt", "x") #create a new txt file
+f = open("news.txt", "a+") #append
 
 history=100000  #number of links you want to check
                 #important note: a lot of links will be empty 
+"""
+Webscrape text of the Australian news website www.abc.net.au/news
+@author: roelientimmer
+"""
 
-count=0 #the number of articles found
+count=2382 #the number of articles found
 
-for i in range((12248344-history),12248345):
-    
-    URL = URL_start+str(i) 
-    page=requests.get(URL)
-    soup=BeautifulSoup(page.content, 'html.parser')
-    
-    items=soup.find_all('p', class_='_1SzQc')
-    
-    if items:
-        f.write('\n \n') #skip two lines
-        count=count+1 #the number of articles found
-    
-    for item in items:
+for i in range((12210593-history),12248345):
+    try:
+        URL = URL_start+str(i) 
+        page=requests.get(URL)
+        soup=BeautifulSoup(page.content, 'html.parser')
+        items=soup.find_all('p', class_='_1SzQc')
+        if items:
+            f.write('\n \n') #skip two lines
+            count=count+1 #the number of articles found
         try:
-            f.write(item.text)
-            f.write('\n')
+            for item in items:   
+                f.write(item.text)
+                f.write('\n')
         except:
             winsound.Beep(2500,1000) #beep noise to notify that writing went wrong
+            print('Something with text writing went wrong.')
+            print('See article nr.: ',i)
+    except:
+        winsound.Beep(2500,1000) #beep noise to notify that writing went wrong
+        print('Something with beautifulsoup went wrong.')
+        print('See article nr.: ',i)
             
 f.close() #close txt file
   
